31643b885f86b5aa90d9a9e42798192a73729fc7
jasmineojeda01 <57571541+jasmineojeda01@users.noreply.github.com>
2022-04-10 10:52:20 -0700

    Added csv files

--- api/HelloApiHandler.py
+++ api/HelloApiHandler.py
@@ -4,7 +4,7 @@ class HelloApiHandler(Resource):
   def get(self):
     return {
       'resultStatus': 'SUCCESS',
-      'message': "Hello Api Handler"
+      'message': "Hello from the Back-end!!"
       }
 
   def post(self):
