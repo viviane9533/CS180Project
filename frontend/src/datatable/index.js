394bc5a5be1412f226b175caf921571a2ea04129
jasmineojeda01 <57571541+jasmineojeda01@users.noreply.github.com>
2022-04-21 19:02:46 -0700

    Moved location of Add button

--- frontend/src/datatable/index.js
+++ frontend/src/datatable/index.js
@@ -19,59 +19,61 @@ export default function Datatable({ data, deleteTableRows, addTableRows }) {
     }
 
     return (
-    <table className = "dataTable" cellPadding={0} cellspacing={50}>
-        <thead>
-            <tr> 
-                {data[0].map((heading) => <th> {heading} </th>)}
-                
-            </tr>
-        </thead>
-        <tbody>
-            {data_body.map((row,index) => (
-                
-                <tr key={index}>
-                    {columns.map((column) => (<td>{row[column]}</td>
-                    ))}
-                    <td><button className="btn btn-outline-danger" onClick={()=>(deleteTableRows(index))}>x</button></td>
-                    <td><button className="btn btn-outline-danger" onClick={()=>setButtonAddPopup(true)}>Add</button></td>
+    <div>
+        <button className="btn btn-outline-danger" onClick={()=>setButtonAddPopup(true)}>Add</button>
+        <table className = "dataTable" cellPadding={0} cellspacing={50}>
+            <thead>
+                <tr> 
+                    {data[0].map((heading) => <th> {heading} </th>)}
+                    
                 </tr>
-                
-            ))}
-        </tbody>
-        <Popup trigger={getButtonAddPopup} setTrigger={setButtonAddPopup}>
-            <h3>Add a Player</h3>
-            <form className="form">
-                <label> Player name: </label>
-                <input
-                    type="text"
-                    required
-                    value={getPlayerName}
-                    onChange={(e) => setPlayerName(e.target.value)}
-                />
-                <label> Team ID: </label>
-                <input
-                    type="text"
-                    required
-                    value={getTeamID}
-                    onChange={(e) => setTeamID(e.target.value)}
-                />
-                <label> Player ID: </label>
-                <input
-                    type="text"
-                    required
-                    value={getPlayerID}
-                    onChange={(e) => setPlayerID(e.target.value)}
-                />
-                <label> Season: </label>
-                <input
-                    type="text"
-                    required
-                    value={getSeason}
-                    onChange={(e) => setSeason(e.target.value)}
-                />
-                <button onClick={()=>addTableRows(getPlayerName, getTeamID, getPlayerID, getSeason)}>Add Player</button>
-            </form>
-        </Popup>
-    </table>
+            </thead>
+            <tbody>
+                {data_body.map((row,index) => (
+                    
+                    <tr key={index}>
+                        {columns.map((column) => (<td>{row[column]}</td>
+                        ))}
+                        <td><button className="btn btn-outline-danger" onClick={()=>(deleteTableRows(index))}>x</button></td>
+                    </tr>
+                    
+                ))}
+            </tbody>
+            <Popup trigger={getButtonAddPopup} setTrigger={setButtonAddPopup}>
+                <h3>Add a Player</h3>
+                <form className="form">
+                    <label> Player name: </label>
+                    <input
+                        type="text"
+                        required
+                        value={getPlayerName}
+                        onChange={(e) => setPlayerName(e.target.value)}
+                    />
+                    <label> Team ID: </label>
+                    <input
+                        type="text"
+                        required
+                        value={getTeamID}
+                        onChange={(e) => setTeamID(e.target.value)}
+                    />
+                    <label> Player ID: </label>
+                    <input
+                        type="text"
+                        required
+                        value={getPlayerID}
+                        onChange={(e) => setPlayerID(e.target.value)}
+                    />
+                    <label> Season: </label>
+                    <input
+                        type="text"
+                        required
+                        value={getSeason}
+                        onChange={(e) => setSeason(e.target.value)}
+                    />
+                    <button onClick={()=>addTableRows(getPlayerName, getTeamID, getPlayerID, getSeason)}>Add Player</button>
+                </form>
+            </Popup>
+        </table>
+    </div>
     )
 }
