114a461a6eaf1dd6b55cf52d7ae4050106614644
Kenneth <kgela001@ucr.edu>
2022-05-12 01:22:25 -0700

    fixed search with pagination
    BUG:
    search is functional, however, you need to change pages to see the search come to effect.
    
    currently testing different search implementation

--- frontend/src/datatable/index.js
+++ frontend/src/datatable/index.js
@@ -1,9 +1,13 @@
 import React from "react"
 import '../index.css'
 import Popup from "./add_popup.js"
-import { useState } from 'react'
+import { useState , useMemo } from 'react'
 import { Bar } from "react-chartjs-2";
 import { Line } from "react-chartjs-2";
+import '../Pagination/style.scss';
+import Pagination from "../Pagination/Pagination";
+import '../Pagination/pagination.scss'
+let PageSize = 25;
 
 export default function Datatable({ data, deleteTableRows, addTableRows, topPlayer }) {
     const [getButtonAddPopup, setButtonAddPopup] = useState(false)
@@ -14,12 +18,21 @@ export default function Datatable({ data, deleteTableRows, addTableRows, topPlay
     const [getPlayerID, setPlayerID] = useState("")
     const [getSeason, setSeason] = useState("")
 
+    const [currentPage, setCurrentPage] = useState(1);
+
+
     const columns = Object.keys(data[0])
     var data_body = new Array()
 
     for (var i=1; i < data.length; i++) {
         data_body[i] = data[i]
     }
+
+     const currentTableData = useMemo(() => {
+        const firstPageIndex = (currentPage - 1) * PageSize;
+        const lastPageIndex = firstPageIndex + PageSize;
+        return data_body.slice(firstPageIndex, lastPageIndex);
+      }, [currentPage]);
 
     return (
     <div>
@@ -35,7 +48,7 @@ export default function Datatable({ data, deleteTableRows, addTableRows, topPlay
                 </tr>
             </thead>
             <tbody>
-                {data_body.map((row,index) => (
+                {currentTableData.map((row,index) => (
                     
                     <tr key={index}>
                         {columns.map((column) => (<td>{row[column]}</td>
@@ -83,6 +96,13 @@ export default function Datatable({ data, deleteTableRows, addTableRows, topPlay
 
             
         </table>
+        <Pagination
+    className="pagination-bar"
+    currentPage={currentPage}
+    totalCount={data_body.length}
+    pageSize={PageSize}
+    onPageChange={page => setCurrentPage(page)}
+  />
     </div>
     
     )
